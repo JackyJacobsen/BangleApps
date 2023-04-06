d3b2921f90df6fbe68b02c06f696c109544ed800
jacob <jacob.hoeck@uibk.ac.at>
2023-04-06 20:04:26 +0200

    accelstream: fix interval; button reset

--- apps/accelstream/app.js
+++ apps/accelstream/app.js
@@ -1,8 +1,10 @@
+var intervalId;
+
 function setAccelHighOutput() {
   // Bangle.accelWr(0x18,0b01110100); // off, +-8g
   // Bangle.accelWr(0x1B,0x07 | 0x40); // 1600hz output, ODR/2 filter
   // Bangle.accelWr(0x18,0b11110100); // +-8g
-  // Bangle.setPollInterval(0.625); // 1600hz input
+  // Bangle.setPollInterval(10); // 1600hz input
 
   Bangle.accelWr(0x18,0b01110100); // off, +-8g
   Bangle.accelWr(0x1B,0x03 | 0x40); // 100hz output, ODR/2 filter
@@ -17,7 +19,8 @@ function resetAccelOutput() {
   Bangle.accelWr(0x18,0b11101100); // +-4g
 }
 
-function accelBluetoothHandler(a) {
+function accelBluetoothHandler() {
+  var a = Bangle.getAccel();
   var d = [
     "A",
     +a.x.toFixed(5),
@@ -30,35 +33,24 @@ function accelBluetoothHandler(a) {
 }
 
 function exit() {
-  Bangle.removeListener('accel', accelBluetoothHandler);
-  // resetAccelOutput();
+  clearInterval(intervalId);
+  //Bangle.removeListener('accel', accelBluetoothHandler);
+  resetAccelOutput();
   load();
-}
-
-function render() {
-  Bangle.drawWidgets();
-
-  var Layout = require("Layout");
-  var layout = new Layout({
-    btns:[{
-      id: "btnExit", 
-      label:/*LANG*/"EXIT", 
-      cb:()=>{
-        exit();
-      }
-    }]
-  });
-  layout.render();
 }
 
 function init() {
   g.clear();
-  render();
-  // setWatch(function() {
-  //   exit();
-  // }, BTN2);
-  // setAccelHighOutput();
-  Bangle.on('accel', accelBluetoothHandler);
+  setWatch(() => {
+    exit();
+  }, BTN);
+  setAccelHighOutput();
+
+  intervalId = setInterval(accelBluetoothHandler, 10);
+
+  //Bangle.on('accel', accelBluetoothHandler);
 }
 
+Bangle.loadWidgets();
+Bangle.drawWidgets();
 init();
