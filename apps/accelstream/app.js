603949de657c79a8e596c6e9d1a0f9dbfd25d344
jacob <jacob.hoeck@uibk.ac.at>
2023-04-06 11:44:26 +0200

    change value rounding

--- apps/accelstream/app.js
+++ apps/accelstream/app.js
@@ -3,11 +3,11 @@ g.clear();
 Bangle.on('accel',function(a) {
   var d = [
     "A",
-    Math.round(a.x * 100),
-    Math.round(a.y * 100),
-    Math.round(a.z * 100),
-    Math.round(a.diff * 100),
-    Math.round(a.mag * 100)
+    Math.round((a.x + Number.EPSILON) * 100) / 100,
+    Math.round((a.y + Number.EPSILON) * 100) / 100,
+    Math.round((a.z + Number.EPSILON) * 100) / 100,
+    Math.round((a.diff + Number.EPSILON) * 100) / 100,
+    Math.round((a.mag + Number.EPSILON) * 100) / 100
     ];
   Bluetooth.println(d.join(","));
 })
