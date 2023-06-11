df8ccbc65e1570d75a5c9d0a0ef9ff38f6be1f80
jacob <jacob.hoeck@uibk.ac.at>
2023-06-11 11:25:13 +0200

    accelstream: add stop/startAccelPolling

--- apps/accelstream/app.js
+++ apps/accelstream/app.js
@@ -3,13 +3,15 @@ var hzCounterIntervalId;
 var hzCounter = 0;
 
 function setAccelHighOutput() {
-  Bangle.setPollInterval(4000); // reduce poll interval, as we read the accel values manually
+  Bangle.stopAccelPolling();
+  Bangle.setPollInterval(1000); // reduce poll interval, as we read the accel values manually
   Bangle.accelWr(0x18,0b01110100); // off, +-8g
   Bangle.accelWr(0x1B,0x07 | 0x40); // 1600hz output, ODR/2 filter
   Bangle.accelWr(0x18,0b11110100); // +-8g
 }
 
 function resetAccelOutput() {
+  Bangle.startAccelPolling();
   Bangle.setPollInterval(80); // default poll interval
   Bangle.accelWr(0x18,0b01101100); // off, +-4g
   Bangle.accelWr(0x1B,0x0); // default 12.5hz output
