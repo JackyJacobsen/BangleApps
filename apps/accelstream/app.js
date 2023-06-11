44b56b4d8bde3037b3f20630d3cb189b1c945631
jacob <jacob.hoeck@uibk.ac.at>
2023-06-11 12:39:33 +0200

    accelstream: change range back to +-8g

--- apps/accelstream/app.js
+++ apps/accelstream/app.js
@@ -4,15 +4,10 @@ var hzCounter = 0;
 
 function setAccelHighOutput() {
   Bangle.stopAccelPolling();
-  Bangle.setPollInterval(1000); // reduce poll interval, as we read the accel values manually
-  Bangle.accelWr(0x18,0b01100100); // off, +-2g
+  Bangle.setPollInterval(1000); // reduce poll interval, as we read the accel values manually  
+  Bangle.accelWr(0x18,0b01110100); // off, +-8g
   Bangle.accelWr(0x1B,0x07 | 0x40); // 1600hz output, ODR/2 filter
-  Bangle.accelWr(0x18,0b11100100); // +-2g
-
-  
-  // Bangle.accelWr(0x18,0b01110100); // off, +-8g
-  // Bangle.accelWr(0x1B,0x07 | 0x40); // 1600hz output, ODR/2 filter
-  // Bangle.accelWr(0x18,0b11110100); // +-8g
+  Bangle.accelWr(0x18,0b11110100); // +-8g
 }
 
 function resetAccelOutput() {
